3961d63db06289affcdc7f09bb68d34f4dc06e5c
greenpea88 <green_pea88@naver.com>
2021-12-11 20:26:09 +0900

    :construction: ADD: make getFoodList frame

--- src/main/java/com/bside/whatMeal/controller/api/FoodController.java
+++ src/main/java/com/bside/whatMeal/controller/api/FoodController.java
@@ -3,20 +3,30 @@ package com.bside.whatMeal.controller.api;
 import com.bside.whatMeal.dto.reqdto.FoodPostReqDto;
 import com.bside.whatMeal.service.FoodService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/foodList")
+@RequestMapping("/foods")
 @RequiredArgsConstructor
 public class FoodController {
 
     private final FoodService foodService;
 
-    public void getFoodList(){
+    @GetMapping()
+    public void getFoodList(@RequestParam List<Integer> basic,
+                            @RequestParam int soup,
+                            @RequestParam(required = false) List<Integer> cook,
+                            @RequestParam(required = false) List<Integer> ingredient,
+                            @RequestParam(required = false) List<Integer> state,
+                            @RequestParam int page){
         //조건에 맞는 음식 추천 리스트(5개씩) 가져오기 >> paging 사용
+        //basic, soup는 필수값
+        //basic은 최대 2개까지
+        //soup, ingredient, state 0-2개
+        //내용값이 아닌 id 값을 받아옴
     }
 
     @PostMapping()
