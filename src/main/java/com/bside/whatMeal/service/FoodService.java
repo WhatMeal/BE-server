0ee876ba87d09b80be00585c670619404fe0ec57
greenpea88 <green_pea88@naver.com>
2021-12-12 20:39:00 +0900

    :recycle: REFACTOR: post food 부분 refactor

--- src/main/java/com/bside/whatMeal/service/FoodService.java
+++ src/main/java/com/bside/whatMeal/service/FoodService.java
@@ -2,8 +2,7 @@ package com.bside.whatMeal.service;
 
 import com.bside.whatMeal.domain.entity.Basic;
 import com.bside.whatMeal.domain.entity.Food;
-import com.bside.whatMeal.domain.repository.BasicRepository;
-import com.bside.whatMeal.domain.repository.FoodRepository;
+import com.bside.whatMeal.domain.repository.*;
 import com.bside.whatMeal.dto.reqdto.FoodPostReqDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +16,9 @@ public class FoodService {
 
     private final FoodRepository foodRepository;
     private final BasicRepository basicRepository;
+    private final CookRepository cookRepository;
+    private final IngredientRepository ingredientRepository;
+    private final StateRepository stateRepository;
 
     public void getFoodList(List<Integer> basic,
                             int soup,
@@ -32,12 +34,14 @@ public class FoodService {
     public void postFood(FoodPostReqDto reqDto){
 //        ArrayList<Long> basicList = reqDto.getBasic();
         //Food table에 음식 이름 추가
-        Food newFood = new Food();
-        newFood = foodRepository.save(newFood);
+        Food newFood = Food.builder().food(reqDto.getFood()).soup(reqDto.getSoup()).build();
+//        newFood = foodRepository.save(newFood);
+        //TODO: 없을 경우의 exception 처리 필요
+        reqDto.getBasics().forEach(basic -> newFood.addBasic(basicRepository.findById(basic).orElse(null)));
+        reqDto.getCooks().forEach(cook -> newFood.addCook(cookRepository.findById(cook).orElse(null)));
+        reqDto.getIngredients().forEach(ingredient -> newFood.addIngredient(ingredientRepository.findById(ingredient).orElse(null)));
+        reqDto.getStates().forEach(state -> newFood.addState(stateRepository.findById(state).orElse(null)));
 
-//        없을 경우의 exception 처리 필요
-        Basic basic = basicRepository.findById(reqDto.getBasic()).orElse(null);
-
-        //추가된 내용 관계 테이블에 추가
+        foodRepository.save(newFood);
     }
 }
