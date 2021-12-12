0470e5da4c0e5c3271055606b5e85c1d7e63d41c
greenpea88 <green_pea88@naver.com>
2021-12-12 20:26:21 +0900

    :building_construction: UPDATE: Food entity 연관 관계 연결 이동

--- src/main/java/com/bside/whatMeal/domain/entity/Food.java
+++ src/main/java/com/bside/whatMeal/domain/entity/Food.java
@@ -1,21 +1,73 @@
 package com.bside.whatMeal.domain.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Food {
     //음식 이름
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String food;
     private String soup;
+
+    @ManyToMany
+    @JoinTable(name = "FoodBasic",
+            joinColumns = @JoinColumn(name="food_id"),
+            inverseJoinColumns = @JoinColumn(name="basic_id")
+    )
+    private List<Basic> basics = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "FoodCook",
+            joinColumns = @JoinColumn(name="food_id"),
+            inverseJoinColumns = @JoinColumn(name="cook_id")
+    )
+    private List<Cook> cooks = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "FoodIngredient",
+            joinColumns = @JoinColumn(name="food_id"),
+            inverseJoinColumns = @JoinColumn(name="ingredient_id")
+    )
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "FoodState",
+            joinColumns = @JoinColumn(name="food_id"),
+            inverseJoinColumns = @JoinColumn(name="state_id")
+    )
+    private List<State> states = new ArrayList<>();
+
+    public void addBasic(Basic basic){
+        this.basics.add(basic);
+    }
+
+    public void addCook(Cook cook){
+        this.cooks.add(cook);
+    }
+
+    public void addIngredient(Ingredient ingredient){
+        this.ingredients.add(ingredient);
+    }
+
+    public void addState(State state){
+        this.states.add(state);
+    }
+
+    @Builder
+    public Food(String food, String soup){
+        this.food = food;
+        this.soup = soup;
+    }
 }
