7f210e7e2c1ac0fcb6560f68bebe3c7b17b3283c
yuum <kangum99@naver.com>
2021-12-19 18:35:48 +0900

    :sparkles: NEW: 온보딩 정보 저장 후 생성되는 id값 반환하는 resDto 생성

---
+++ src/main/java/com/bside/whatMeal/dto/resdto/InfoIdResDto.java
@@ -0,0 +1,12 @@
+package com.bside.whatMeal.dto.resdto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InfoIdResDto {
+    //온보딩 이후 생성된 infoId 반환하는 Dto
+    private int id;
+}
+
