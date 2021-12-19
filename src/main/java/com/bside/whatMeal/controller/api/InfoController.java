4694d9cf82da77d2f98e717e3b484338a1371319
yuum <kangum99@naver.com>
2021-12-19 18:33:50 +0900

    :sparkles: NEW: Info Controller 생성

---
+++ src/main/java/com/bside/whatMeal/controller/api/InfoController.java
@@ -0,0 +1,37 @@
+package com.bside.whatMeal.controller.api;
+
+import com.bside.whatMeal.domain.entity.Info;
+import com.bside.whatMeal.dto.reqdto.InfoFinalFoodPostReqDto;
+import com.bside.whatMeal.dto.reqdto.InfoPostReqDto;
+import com.bside.whatMeal.dto.resdto.InfoIdResDto;
+import com.bside.whatMeal.dto.resdto.PostResultResDto;
+import com.bside.whatMeal.service.InfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/infos")
+@RequiredArgsConstructor
+
+public class InfoController {
+    private final InfoService infoService;
+
+    @GetMapping()
+    public List<Info> getInfoList() {
+        return infoService.getInfo();
+    }
+
+    @PostMapping("/onboarding")
+    //온보딩 이후 저장 되는 info
+    public InfoIdResDto addInfoOnboarding(@RequestBody InfoPostReqDto reqDto) {
+        return infoService.addInfoOnboarding(reqDto);
+    }
+
+    @PutMapping("/finalfood")
+    //최종 음식 정한 후 음식 추가 info
+    public PostResultResDto addInfoFinalFood(@RequestBody InfoFinalFoodPostReqDto reqDto) {
+        return infoService.addInfoFinalFood(reqDto);
+    }
+}
