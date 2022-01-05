68411842021119c930d6a6c28deaea4b1340f0cd
yuum <kangum99@naver.com>
2022-01-05 20:08:01 +0900

    :hammer: ADD: info의 age를 Int에서 String으로 바꿈

--- src/main/java/com/bside/whatMeal/dto/reqdto/InfoPostReqDto.java
+++ src/main/java/com/bside/whatMeal/dto/reqdto/InfoPostReqDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class InfoPostReqDto {
 
-    private int age; //나이대
+    private String age; //나이대
     private String mealTime; //식사시간(아침, 점심, 저녁)
     private String standard1; //메뉴 선정 기준1
     private String standard2; //메뉴 선정 기준2
